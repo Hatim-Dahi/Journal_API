c19ec62bb2f4679d56726c1c0f8e81ff060a1c0e
Hatim-Dahi <hatimtaherdahi53@gmail.com>
2024-06-01 21:12:03 +0530

    Journal entity setup

--- src/main/java/google/com/FirstProject/controller/EntryController.java
+++ src/main/java/google/com/FirstProject/controller/EntryController.java
@@ -1,4 +1,25 @@
 package google.com.FirstProject.controller;
 
+import google.com.FirstProject.entity.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
 public class EntryController {
+    private Map<Long, JournalEntity> entries= new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntity> getAll(){
+        return new ArrayList(entries.values());
+    }
+
+    @PostMapping
+    public void createEntry(@RequestBody JournalEntity newEntry){
+        entries.put(newEntry.getId(), newEntry);
+    }
 }
